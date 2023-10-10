3e851e37cb9e6d0009eb41f90bc75dca5fb03627
aaronperona <aarpercar@gmail.com>
2023-10-10 11:21:34 +0200

    Clase Bici implementada

---
+++ estaciones-PeronaCarrillo/src/main/java/dominio/Bici.java
@@ -0,0 +1,48 @@
+package dominio;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Bici {
+	private String id;
+	private String modelo;
+	private LocalDate fechaAlta;
+	private LocalDate fechaBaja;
+	private String motivoBaja;
+	
+	public Bici(String modelo, LocalDate fechaAlta) {
+		id = UUID.randomUUID().toString();
+		this.modelo = modelo;
+		this.fechaAlta = fechaAlta;
+		this.fechaBaja = null;
+		this.motivoBaja = "";		
+	}
+
+	public LocalDate getFechaBaja() {
+		return fechaBaja;
+	}
+
+	public void setFechaBaja(LocalDate fechaBaja) {
+		this.fechaBaja = fechaBaja;
+	}
+
+	public String getMotivoBaja() {
+		return motivoBaja;
+	}
+
+	public void setMotivoBaja(String motivoBaja) {
+		this.motivoBaja = motivoBaja;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public LocalDate getFechaAlta() {
+		return fechaAlta;
+	}
+}
