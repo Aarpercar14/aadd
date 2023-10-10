27f276a512d65f0fc685c78a84115976b565585c
aaronperona <aarpercar@gmail.com>
2023-10-10 10:20:17 +0200

    Primera implementacion de Usuario

---
+++ estaciones-PeronaCarrillo/src/main/java/dominio/Usuario.java
@@ -0,0 +1,53 @@
+package dominio;
+
+import java.time.LocalDate;
+
+public class Usuario {
+	private String nombre;
+	private String apellidos;
+	private String email;
+	private String password;
+	private String telefono;
+	private LocalDate fechaNacimiento;
+	
+	public Usuario(String nombre, String apellidos, String email, String password, String telefono, LocalDate fechaNacimiento) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.email = email;
+		this.password = password;
+		this.telefono = telefono;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}	
+}
