377b33fd11cdb69126b9535fce6e56ca36a2591e
sergioillan <sergioillan@localhost>
2023-10-22 21:07:44 +0200

    Servicios turisticos con las lecturas con DOM de geonames

--- estaciones-PeronaCarrillo/src/main/java/servicio/IServicioSitiosTuristicos.java
+++ estaciones-PeronaCarrillo/src/main/java/servicio/IServicioSitiosTuristicos.java
@@ -2,12 +2,14 @@ package servicio;
 
 import java.util.List;
 
+import org.w3c.dom.NodeList;
+
 import dominio.SitioTuristico;
 
 public interface IServicioSitiosTuristicos {
 
 	
-	List<SitioTuristico> getSitiosInteres(double cordX, double cordY);
+	List<SitioTuristico> getSitiosInteres(int cordX, int cordY);
 	
 	String getInfoSitio(String id);
 }
---
+++ estaciones-PeronaCarrillo/src/main/java/servicio/ServicioSitiosTuristicos.java
@@ -0,0 +1,67 @@
+package servicio;
+
+import repositorio.EntidadNoEncontrada;
+import repositorio.FactoriaRepositorios;
+import repositorio.Repositorio;
+import repositorio.RepositorioException;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import dominio.SitioTuristico;
+
+
+public class ServicioSitiosTuristicos implements IServicioSitiosTuristicos{
+	
+	private Repositorio<SitioTuristico, String> repositorio = FactoriaRepositorios.getRepositorio(SitioTuristico.class);
+
+
+	@Override
+	public List<SitioTuristico> getSitiosInteres(int cordX, int cordY){
+		String sitios="http://api.geonames.org/findNearbyWikipedia?lat="+cordX+"&lng="+cordY+"&username=plasnake";//cambiar usuario
+		DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+		Document documento = null;
+		try {
+			DocumentBuilder analizador = factoria.newDocumentBuilder();
+			 documento = analizador.parse(sitios);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		NodeList nodos=documento.getElementsByTagName("entry");
+		LinkedList<SitioTuristico> lista=new LinkedList<SitioTuristico>();
+		for(int i=0;i<nodos.getLength();i++)
+		{
+			Element elemento= (Element) nodos.item(i);
+			SitioTuristico s=new SitioTuristico(elemento.getElementsByTagName("title").item(0).getLocalName(),elemento.getElementsByTagName("summary").item(0).getLocalName(),Integer.parseInt(elemento.getElementsByTagName("lat").item(0).getLocalName()),Integer.parseInt(elemento.getElementsByTagName("lng").item(0).getLocalName()),elemento.getElementsByTagName("wikipediaUrl").item(0).getLocalName());
+			lista.add(s);
+		}
+		return  lista;
+	}
+
+	@Override
+	public String getInfoSitio(String id) {
+		try {
+			return repositorio.getById(id).toString();
+		} catch (RepositorioException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EntidadNoEncontrada e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
