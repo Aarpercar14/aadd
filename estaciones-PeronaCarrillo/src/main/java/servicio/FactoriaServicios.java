27d907673cad3d59ac6b76d2929cbec77c98b922
sergiosanc16 <sergio.sanchezi@um.es>
2023-10-27 19:17:11 +0200

    avance

--- estaciones-PeronaCarrillo/src/main/java/servicio/FactoriaServicios.java
+++ estaciones-PeronaCarrillo/src/main/java/servicio/FactoriaServicios.java
@@ -10,32 +10,30 @@ import utils.PropertiesReader;
 public class FactoriaServicios {
 
 	private static final String PROPERTIES = "servicios.properties";
-	
+
 	private static Map<Class<?>, Object> servicios = new HashMap<>();
-	
+
 	@SuppressWarnings("unchecked")
-	public static <T> T getServicio(Class<T> servicio){
+	public static <T> T getServicio(Class<T> servicio) {
 
 		try {
-			if(servicios.containsKey(servicio)) {
+			if (servicios.containsKey(servicio)) {
 				return (T) servicios.get(servicio);
-			}
-			else {
+			} else {
 				PropertiesReader properties = new PropertiesReader(PROPERTIES);
 				String clase = properties.getProperty(servicio.getName());
 				T servicioInstancia = (T) Class.forName(clase).getConstructor().newInstance();
 
 				servicios.put(servicio, servicioInstancia);
-				System.out.println("1");
-
 				return servicioInstancia;
+
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 			throw new RuntimeException("No se ha podido obtener la implementación del servicio: " + servicio.getName());
 		}
-		
+
 	}
 }
