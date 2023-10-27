b4ec8791394ac3fc792a57067706105c7bc39e78
sergiosanc16 <sergio.sanchezi@um.es>
2023-10-27 20:25:53 +0200

    avance

--- estaciones-PeronaCarrillo/src/main/java/dominio/RepositorioSitiosTuristicosJSON.java
+++ estaciones-PeronaCarrillo/src/main/java/dominio/RepositorioSitiosTuristicosJSON.java
@@ -4,6 +4,10 @@ import repositorio.RepositorioJSON;
 
 public class RepositorioSitiosTuristicosJSON extends RepositorioJSON<SitioTuristico>{
 	
+	public RepositorioSitiosTuristicosJSON() {
+		super();
+	}
+	
 	@Override
 	public Class<SitioTuristico> getClase() {
 		return SitioTuristico.class;
