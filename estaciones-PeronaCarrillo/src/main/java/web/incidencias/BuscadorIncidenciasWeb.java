965dc4ec73b07079550598bc359d31d8cbd65989
aaronperona <aarpercar@gmail.com>
2023-12-18 13:58:29 +0100

    Creacion de la estructura de las vistas 2

---
+++ estaciones-PeronaCarrillo/src/main/java/web/incidencias/BuscadorIncidenciasWeb.java
@@ -0,0 +1,14 @@
+package web.incidencias;
+
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+public class BuscadorIncidenciasWeb {
+	
+	@Inject
+	protected FacesContext facesContext;
+	
+	public void gestionarIncidencia() {
+		
+	}
+}
--- estaciones-PeronaCarrillo/src/main/java/web/main/MainGestorWeb.java
+++ estaciones-PeronaCarrillo/src/main/java/web/main/MainGestorWeb.java
@@ -1,7 +1,33 @@
 package web.main;
 
+import java.io.IOException;
 import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 public class MainGestorWeb implements Serializable{
 
+	
+	@Inject
+	protected FacesContext facesContext;
+	
+	public void verInicidencias() {
+		try {
+			facesContext.getExternalContext().redirect("buscadorIncidencias.xhtml");
+		} catch (IOException e) {
+			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "No se ha podido navegar"));
+			e.printStackTrace();
+		}
+	}
+	
+	public void load() {
+		try {
+			facesContext.getExternalContext().redirect("mainGestor.xhtml");
+		} catch (IOException e) {
+			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "No se ha podido navegar"));
+			e.printStackTrace();
+		}
+	}
 }
